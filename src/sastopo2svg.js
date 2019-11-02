444b14dc9bc2ff1764bcca94f4dca1547755a775
Rob Johnston <rob.johnston@joyent.com>
2019-11-01 17:04:10 -0700

    Add code to dynamically build/show PHY link error table when port nodes are clicked on

--- src/sastopo2svg.js
+++ src/sastopo2svg.js
@@ -38,22 +38,34 @@ function showInfo(evt) {
     var nodeinfo = document.getElementById("nodeinfo");
     var numrows = nodeinfo.rows.length;
     for (var i = 0; i < numrows; i++) {
-        console.log("deleting row ...");
         nodeinfo.deleteRow(-1);
     }
 
+    // Clear and hide the PHY err table
+    var errtable = document.getElementById("errtable");
+    errtable.hidden = true;
+    var errinfo = document.getElementById("errinfo");
+    numrows = errinfo.rows.length;
+    for (var i = 0; i < numrows; i++) {
+        errinfo.deleteRow(-1);
+    }
+
     var group = evt.target.parentElement;
+    var link_err_props = ["invalid-dword", "running-disparity-error",
+        "loss-dword-sync", "reset-problem"];
     var props;
     var name = group.getAttribute("name");
 
     if (name === "initiator") {
-        props = ["fmri", "hc-fmri", "devfs-name", "name", "manufacturer", "model", "serial", "label"];
+        props = ["fmri", "hc-fmri", "devfs-name", "name", "manufacturer",
+            "model", "serial", "label"];
     } else if (name === "port") {
         props = ["fmri", "name", "local-sas-address", "attached-sas-address"];
     } else if (name === "expander") {
         props = ["fmri", "name", "devfs-name"];
     } else if (name === "target") {
-        props = ["fmri", "hc-fmri", "name", "manufacturer", "model", "serial", "label"];
+        props = ["fmri", "hc-fmri", "name", "manufacturer", "model", "serial",
+            "label"];
     }
 
     for (const prop of props) {
@@ -71,7 +83,61 @@ function showInfo(evt) {
         var row = nodeinfo.insertRow(-1);
         var fieldcell = row.insertCell(-1);
         fieldcell.innerHTML = prop;
+        fieldcell.style.bold = true;
         var valuecell = row.insertCell(-1);
+        valuecell.colSpan = 4;
         valuecell.innerHTML = value;
+    }
+
+    if (name == "port") {
+        //
+        // Determine the number of phys on this port by scraping out the
+        // start_phy and end_phy fields from the authority portion of this
+        // node's sas-scheme FMRI.
+        //
+        var fmri = group.getAttribute("fmri");
+        var regex = /start-phy=(\d+):end-phy=(\d+)/g;
+        var match = regex.exec(fmri);
+        num_phys = (match[2] - match[1]) + 1;
+
+        var phys = [];
+        for (i = 0; i < num_phys; i++) {
+            phys[i] = {
+                [link_err_props[0]]: [],
+                [link_err_props[1]]: [],
+                [link_err_props[2]]: [],
+                [link_err_props[3]]: []
+            };
+        }
+        for (const prop of link_err_props) {
+            var value = group.getAttribute(prop);
+            if (value === undefined) {
+                return;
+            }
+            var value = group.getAttribute(prop).split(",");
+            for (phy = 0; phy < value.length; phy++) {
+                phys[phy][prop].push(value[phy]);
+            }
+        }
+
+        // Unhide the PHY Error table
+        errtable.hidden = false;
+
+        var hdrrow = errinfo.insertRow(-1);
+        var hdrcell = hdrrow.insertCell(-1);
+        hdrcell.innerHTML = "PHY #";
+        for (const prop of link_err_props) {
+            hdrcell = hdrrow.insertCell(-1);
+            hdrcell.innerHTML = prop;
+        }
+        for (i = 0; i < num_phys; i++) {
+            var errrow = errinfo.insertRow(-1);
+            var errcell = errrow.insertCell(-1)
+            errcell.innerHTML = i;
+            for (const prop of link_err_props) {
+                errcell = errrow.insertCell(-1);
+                errcell.innerHTML = phys[i][prop];
+            }
+        }
     }
 }
